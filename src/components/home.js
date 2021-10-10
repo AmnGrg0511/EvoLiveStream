6283d5927f5090f8b1aab0da8031ebcc0d487bd2
AmnGrg05 <amanbtech05@gmail.com>
2021-10-10 19:06:26 +0530

    added features

--- src/components/home.js
+++ src/components/home.js
@@ -1,57 +1,54 @@
-import { ImDisplay } from 'react-icons/im';
-import { Link } from 'react-router-dom';
-import './../styles/home.css'
-function Home(){
-    return(
-        <>
-        <div className="video-main">
-        <video src="./images/Evointrovideo.mp4" className="video" autoPlay muted loop></video>
+import { Link } from "react-router-dom";
+import "./../styles/home.css";
+import { Feature } from "./feature";
+import { greatFeatures, features } from "../data/features.json";
+
+function Home() {
+  const { media, heading, points } = greatFeatures;
+  return (
+    <>
+      <div className="video-main">
+        <video
+          src="./images/Evointrovideo.mp4"
+          className="video"
+          autoPlay
+          muted
+          loop
+        />
         <div className="video-content">
           <h1 className="vc-go">Go Live with Evo</h1>
           <h2 className="vc-l">LIVE STREAMING PLATFORM</h2>
-          <Link><button className="gs">Get started</button></Link>
+          <Link>
+            <button className="gs">Get started</button>
+          </Link>
         </div>
+      </div>
+      <div className="tagline">
+        <div className="th">
+          <h1 className="thc">
+            Level up your live video with reliable, high-quality streaming for
+            your viewers everywhere.
+          </h1>
         </div>
-       <div className="tagline">
-            <div className="th">
-            <h1 className="thc">
-            Level up your live video with reliable, high-quality streaming for your viewers everywhere.
-            </h1>
+      </div>
+      <div className="greatfeature-main">
+        <div className="gfsub front">
+          <img src={media.url} className="featureimg" alt="" />
+          <h2>{heading}</h2>
+        </div>
+        <div className="points-sub">
+          {points.map((point) => (
+            <div className="points-sub2">
+              <img src="./images/check.png" className="pointicon" alt="" />
+              <p className="points-f">{point}</p>
             </div>
-       </div>
-       <div className="greatfeature-main">
-            <div className="gfsub front">
-                  <img src="./images/featured.png" className="featureimg"></img>
-                   <h2>Evo’s live streaming platform works great for</h2>
-            </div>
-            <div className="points-sub"> 
-                      <div className="points-sub2">
-                      <img src="./images/check.png" className="pointicon"></img>
-                      <p className="points-f">Performances and events</p>
-                      </div>
-                      <div className="points-sub2">
-                      <img src="./images/check.png" className="pointicon"></img>
-                      <p className="points-f">Webinars and classes</p>
-                      </div>
-                      <div className="points-sub2">
-                      <img src="./images/check.png" className="pointicon"></img>
-                      <p className="points-f">Community engagement</p>
-                      </div>
-                      <div className="points-sub2">
-                      <img src="./images/check.png" className="pointicon"></img>
-                      <p className="points-f">Company announcements</p>
-                      </div>
-                      <div className="points-sub2">
-                      <img src="./images/check.png" className="pointicon"></img>
-                      <p className="points-f">Product launches</p>
-                      </div>
-                      <div className="points-sub2">
-                      <img src="./images/check.png" className="pointicon"></img>
-                      <p className="points-f">Virtual conferences and panels</p>
-                      </div>
-                </div>
-       </div>
-        </>
-    )
+          ))}
+        </div>
+      </div>
+      {features.map((feature, i) => (
+        <Feature key={i} index={i} {...feature} />
+      ))}
+    </>
+  );
 }
 export default Home;
