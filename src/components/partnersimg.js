9aafa2d441d49a3971599baf7a2b0685b3facc0d
Manish Reddy <76482822+manishreddy6305@users.noreply.github.com>
2021-11-02 15:12:31 +0530

    Delete src directory

--- src/components/partnersimg.js
+++
@@ -1,9 +0,0 @@
-import './../styles/partnersimg.css'
-function Partnersimg(props){
-    return(
-        <>
-            <img src={props.imgurl} className="partnersimg"></img>
-        </>
-    )
-}
-export default Partnersimg
